1b30650ae1f4d3713a7afecfda781dae936cc75d
joacolevy <49836427@est.ort.edu.ar>
2026-05-29 09:06:49 -0300

    trgh

---
+++ Assets/scripts/INTERACTIVEAREA.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class INTERACTIVEAREA : MonoBehaviour
+{
+    public scoreUIManager scoreUI;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.CompareTag("picable"))
+        {
+            Destroy(col.gameObject);
+
+            scoreUI.AddPointAndUpdateScore();
+        }
+    }
+}
---
+++ Assets/scripts/scoreUIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class scoreUIManager : MonoBehaviour
+{
+    public TextMeshProUGUI txt_score;
+
+    public int score = 0;
+
+    void Start()
+    {
+        txt_score.text = "Puntos: 0";
+    }
+
+    public void AddPointAndUpdateScore()
+    {
+        score += 5;
+
+        if(score >= 10)
+        {
+            txt_score.text = "Ganaste";
+        }
+        else
+        {
+            txt_score.text = "Puntos: " + score;
+        }
+    }
+}
