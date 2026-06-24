f44b62ce8e926d447c6a499cfecbce7ca058216c
joacolevy <49836427@est.ort.edu.ar>
2026-06-24 14:32:03 -0300

    asdfgyhuio

---
+++ Assets/scripts/FlickerLight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickerLight : MonoBehaviour
+{
+    public Light luz;
+    public float tiempoMin = 0.05f;
+    public float tiempoMax = 0.3f;
+
+    void Start()
+    {
+        if (luz == null)
+            luz = GetComponent<Light>();
+
+        StartCoroutine(Titilar());
+    }
+
+    IEnumerator Titilar()
+    {
+        while (true)
+        {
+            luz.enabled = !luz.enabled;
+
+            float tiempo = Random.Range(tiempoMin, tiempoMax);
+            yield return new WaitForSeconds(tiempo);
+        }
+    }
+}
